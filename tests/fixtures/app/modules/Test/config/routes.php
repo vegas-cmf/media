031630fb28912210134c2547f0d9873162d3e9f5
tborodziuk <tborodziuk.amstd@gmail.com>
2015-02-13 10:56:56 +0100

    tests update

--- tests/fixtures/app/modules/Test/config/routes.php
+++ tests/fixtures/app/modules/Test/config/routes.php
@@ -1,48 +1,5 @@
 <?php
 return array(
-    'test' => array(
-        'route' => '/test/front/:action/:params',
-        'paths' => array(
-            'module'    =>  'Test',
-            'controller' => 'Frontend\Fake',
-            'action' => 1,
-            'params' => 2
-        )
-    ),
-
-    'testfoo' => array(
-        'route' => '/testfoo/front/:action/:params',
-        'paths' => array(
-            'module'    =>  'Test',
-            'controller' => 'Frontend\Foo',
-            'action' => 1,
-            'params' => 2
-        )
-    ),
-
-    'testfake' => array(
-        'route' => '/test/fake/test',
-        'paths' => array(
-            'module'    =>  'Test',
-            'controller' => 'Backend\Fake',
-            'action' => 'test'
-        ),
-
-        'params' => array(
-            'auth' => 'authUser'
-        )
-    ),
-
-    'testbrokencrud' => array(
-        'route' => '/test/brokencrud/:action/:params',
-        'paths' => array(
-            'module' => 'Test',
-            'controller' => 'Backend\BrokenCrud',
-            'action' => 1,
-            'params' => 2
-        )
-    ),
-
     'testcrud' => array(
         'route' => '/test/crud/:action/:params',
         'paths' => array(
@@ -51,20 +8,6 @@ return array(
             'action' => 1,
             'params' => 2
         )
-    ),
-
-    'testshort' => array(
-        'route' => '/test/short/namespace/:action/:params',
-        'paths' => array(
-            'module'    =>  'Test',
-            'controller' => 'Fake',
-            'action' => 1,
-            'params' => 2
-        ),
-
-        'params' => array(
-            'auth' => 'authUser'
-        )
-    ),
+    )
 
 );
