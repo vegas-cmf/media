38d0c8eaa5ad510ca0db1b09bf085fdbb7f2ee9c
Adrian Malik <adrian.malik.89@gmail.com>
2015-03-10 10:03:06 +0100

    Update jquery bug

--- assets/js/lib/vegas/ui/upload.js
+++ assets/js/lib/vegas/ui/upload.js
@@ -699,27 +699,24 @@ Uploader.Html = function() {
     };
 };
 
-
-
-jQuery.fn.upload = function(construct) {
-    var params = (new Uploader.Filter()).filterParams(construct);
-    var browser = new Uploader.Browser(params.browser);
-    var preview = new Uploader.Preview(params.preview);
-    var input = (new Uploader.Input(this[0], params));
-
-    input.hide().attachOnChange(preview);
-
-    browser.render();
-    browser.attachOnClick(input);
-    browser.attachOnDrop(preview);
-    browser.attachOnDragEnter();
-    browser.attachOnDragOver();
-
-    preview.renderContainer();
-};
-
-
 $(document).ready(function() {
+    jQuery.fn.upload = function(construct) {
+        var params = (new Uploader.Filter()).filterParams(construct);
+        var browser = new Uploader.Browser(params.browser);
+        var preview = new Uploader.Preview(params.preview);
+        var input = (new Uploader.Input(this[0], params));
+
+        input.hide().attachOnChange(preview);
+
+        browser.render();
+        browser.attachOnClick(input);
+        browser.attachOnDrop(preview);
+        browser.attachOnDragEnter();
+        browser.attachOnDragOver();
+
+        preview.renderContainer();
+    };
+
     $('[vegas-cmf="upload"]').each(function() {
         var that = $(this);
         var render = null;
